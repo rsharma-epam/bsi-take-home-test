854bfb48b1b8878b41619d5a7540c2f76810bf10
ravi_sharma2 <ravi_sharma2@epam.com>
2024-06-28 16:24:10 +0530

    Minor refactoring

--- src/test/java/ceo/dog/api/APITest.java
+++ src/test/java/ceo/dog/api/APITest.java
@@ -15,35 +15,34 @@ public class APITest {
     @Test(description = "test GET request to the Dog API endpoint that provides a random dog image")
     public void testRandomImage() {
         Response response = RestAssured.get("https://dog.ceo/api/breeds/image/random");
-        assertThat(response.getStatusCode()).isEqualTo(200);
-        assertThat(response.getContentType()).isEqualTo("application/json");
+        commonChecks(response);
         response.then().body("message", startsWith("https://images.dog.ceo"), "message", endsWith(".jpg"));
-        response.then().body("status", is("success"));
     }
 
     @Test(description = "test GET request to the Dog API endpoint that lists all dog breeds")
     public void testListAllBreeds() {
         Response response = RestAssured.get("https://dog.ceo/api/breeds/list/all");
-        response.then().statusCode(200);
-        response.then().contentType("application/json");
+        commonChecks(response);
 
         response.then().body("message", hasKey("affenpinscher"));
         response.then().body("message", hasKey("beagle"));
         response.then().body("message", hasKey("wolfhound"));
-
-        response.then().body("status", is("success"));
     }
 
     @Test(description = "test GET request to the Dog API endpoint that provides images for the specified sub-breed")
     public void testRetrieveSubBreedImages() {
         Response response = RestAssured.get("https://dog.ceo/api/breed/hound/afghan/images");
-        response.then().statusCode(200);
-        response.then().contentType("application/json");
-        response.then().body("status", is("success"));
+        commonChecks(response);
 
         response.then().body("message", hasItems("https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
                 , "https://images.dog.ceo/breeds/hound-afghan/n02088094_1259.jpg"
                 , "https://images.dog.ceo/breeds/hound-afghan/n02088094_988.jpg"));
 
+    }
+
+    private static void commonChecks(Response response) {
+        response.then().statusCode(200);
+        response.then().contentType("application/json");
+        response.then().body("status", is("success"));
     }
 }
