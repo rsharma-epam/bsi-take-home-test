5c65f17fb86f05dcdedeb44034d0d9c59a9038ab
ravi_sharma2 <ravi_sharma2@epam.com>
2024-06-28 16:27:35 +0530

    Add restassured config

--- src/test/java/ceo/dog/api/APITest.java
+++ src/test/java/ceo/dog/api/APITest.java
@@ -2,26 +2,28 @@ package ceo.dog.api;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class APITest {
 
+    @BeforeClass
+    public void setup(){
+        RestAssured.baseURI = "https://dog.ceo";
+    }
+
     @Test(description = "test GET request to the Dog API endpoint that provides a random dog image")
     public void testRandomImage() {
-        Response response = RestAssured.get("https://dog.ceo/api/breeds/image/random");
+        Response response = RestAssured.get("/api/breeds/image/random");
         commonChecks(response);
         response.then().body("message", startsWith("https://images.dog.ceo"), "message", endsWith(".jpg"));
     }
 
     @Test(description = "test GET request to the Dog API endpoint that lists all dog breeds")
     public void testListAllBreeds() {
-        Response response = RestAssured.get("https://dog.ceo/api/breeds/list/all");
+        Response response = RestAssured.get("/api/breeds/list/all");
         commonChecks(response);
 
         response.then().body("message", hasKey("affenpinscher"));
@@ -31,7 +33,7 @@ public class APITest {
 
     @Test(description = "test GET request to the Dog API endpoint that provides images for the specified sub-breed")
     public void testRetrieveSubBreedImages() {
-        Response response = RestAssured.get("https://dog.ceo/api/breed/hound/afghan/images");
+        Response response = RestAssured.get("/api/breed/hound/afghan/images");
         commonChecks(response);
 
         response.then().body("message", hasItems("https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
